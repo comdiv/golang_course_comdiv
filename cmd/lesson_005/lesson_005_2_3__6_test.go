364488390a56d7c643abc7df5b0d4c4aa953e124
comdiv <fagim.sadykov@gmail.com>
2021-05-21 04:30:23 +0500

    (lesson_005) переделано по замечанию, что нужен табличный тест

--- cmd/lesson_005/lesson_005_2_3__6_test.go
+++ cmd/lesson_005/lesson_005_2_3__6_test.go
@@ -1,11 +1,30 @@
 package lesson_005
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
+
+type exchangeCase struct {
+	x1 int
+	x2 int
+}
 
 func TestExchangePointer(t *testing.T) {
-	var x1, x2 = 2, 4
-	exchangePointer(&x1, &x2)
-	if x1 != 4 || x2 != 2 {
-		t.Errorf("Expected 4, 2 but was %d, %d", x1, x2)
+	exchangeCases := []*exchangeCase{
+		{1, 2},
+		{3, 4},
+		{5, 6},
+		{rand.Intn(100), rand.Intn(200)},
+		{rand.Intn(100), rand.Intn(200)},
+		{rand.Intn(100), rand.Intn(200)},
+		{rand.Intn(100), rand.Intn(200)},
+	}
+	for _, et := range exchangeCases {
+		var x1, x2 = et.x1, et.x2
+		exchangePointer(&x1, &x2)
+		if x1 != et.x2 || x2 != et.x1 {
+			t.Errorf("Expected %d, %d but was %d, %d", et.x2, et.x1, x1, x2)
+		}
 	}
 }
