04edcc976223f93518e432372c14e6b493edb85c
comdiv <fagim.sadykov@gmail.com>
2021-06-22 21:45:20 +0500

    (ta) добавлены параметры для подключения http режима

--- internal/textanalyzer/app/TextAnalyzerArgs.go
+++ internal/textanalyzer/app/TextAnalyzerArgs.go
@@ -1,34 +1,44 @@
 package app
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 type TextAnalyzerArgs struct {
-	size     *int
-	minlen   *int
-	useFirst *bool
-	useLast  *bool
-	nonfreq  *bool
-	json     *bool
-	cpuprof  *string
-	memprof  *string
+	size      *int
+	minlen    *int
+	useFirst  *bool
+	useLast   *bool
+	nonfreq   *bool
+	json      *bool
+	cpuprof   *string
+	memprof   *string
+	debug     *bool
+	http      *int
+	pprofhttp *int
 }
 
 
-
-func (t *TextAnalyzerArgs) Json() bool {
-	return *t.json
-}
 
 func NewTextAnalyzerArgsF() *TextAnalyzerArgs {
+	ENV_DEBUG, exists := os.LookupEnv("DEBUG")
+	var env_debug bool
+	if exists {
+		env_debug = ENV_DEBUG == "true"
+	}
 	return &TextAnalyzerArgs{
-		size:     flag.Int("size", 10, "Collect top SIZE frequent words"),
-		minlen:   flag.Int("minlen", 4, "Min length for word in symmbols"),
-		useFirst: flag.Bool("first", false, "Include first words of statements"),
-		useLast:  flag.Bool("last", false, "Include last words of statements"),
-		nonfreq:  flag.Bool("nonfreq", false, "Less frequent, not more frequent"),
-		json:     flag.Bool("json", false, "Treat file as JSON with standard schema, not as plain"),
-		cpuprof:  flag.String("cpuprof", "", "File for storing CPU profiler info"),
-		memprof:  flag.String("memprof", "", "File for storing Mem profiler info"),
+		size:      flag.Int("size", 10, "Collect top SIZE frequent words"),
+		minlen:    flag.Int("minlen", 4, "Min length for word in symmbols"),
+		useFirst:  flag.Bool("first", false, "Include first words of statements"),
+		useLast:   flag.Bool("last", false, "Include last words of statements"),
+		nonfreq:   flag.Bool("nonfreq", false, "Less frequent, not more frequent"),
+		json:      flag.Bool("json", false, "Treat file as JSON with standard schema, not as plain"),
+		cpuprof:   flag.String("cpuprof", "", "File for storing CPU profiler info"),
+		memprof:   flag.String("memprof", "", "File for storing Mem profiler info"),
+		debug:     flag.Bool("debug", env_debug, "Debug mode - NOT CLEAR HOW TO USE"),
+		http:      flag.Int("http", -1, "If set - port to listen in http mode"),
+		pprofhttp: flag.Int("pprofhttp", -1, "If set - port to listen for http prof"),
 	}
 }
 
@@ -62,4 +72,20 @@ func (t *TextAnalyzerArgs) Cpuprof() string {
 
 func (t *TextAnalyzerArgs) Memprof() string {
 	return *t.memprof
+}
+
+func (t *TextAnalyzerArgs) Debug() bool {
+	return *t.debug
+}
+
+func (t *TextAnalyzerArgs) Http() int {
+	return *t.http
+}
+
+func (t *TextAnalyzerArgs) Pprofhttp() int {
+	return *t.pprofhttp
+}
+
+func (t *TextAnalyzerArgs) Json() bool {
+	return *t.json
 }
