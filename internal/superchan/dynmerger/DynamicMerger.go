90bb0c55a42e28190657bb6054aa8ddeec025f82
comdiv <fagim.sadykov@gmail.com>
2021-06-30 00:20:04 +0500

    (superchan) dynmerger stabilized

--- internal/superchan/dynmerger/DynamicMerger.go
+++ internal/superchan/dynmerger/DynamicMerger.go
@@ -12,53 +12,69 @@ type DynamicMerger struct {
 	out        chan string
 	idgen      int
 	locker     sync.RWMutex
+	wg         sync.WaitGroup
+	stopper    chan struct{}
 }
 
 func New(inputs []chan string, out chan string) *DynamicMerger {
-	result := &DynamicMerger{selectsMap: make(map[int]reflect.SelectCase), selects: make([]reflect.SelectCase, 0), out: out}
+	result := &DynamicMerger{selectsMap: make(map[int]reflect.SelectCase), selects: make([]reflect.SelectCase, 0), out: out, stopper: make(chan struct{})}
 	for _, ch := range inputs {
-		_ = result.Register(ch)
+		result.directRegister(ch)
 	}
 	result.Start()
 	return result
+}
+
+func (m *DynamicMerger) directRegister(ch chan string){
+	m.idgen++
+	selectCase := reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ch)}
+	m.selectsMap[m.idgen] = selectCase
+	m.selects = append(m.selects, selectCase)
 }
 
 // регистрирует новый канал и возвращает его токн
 func (m *DynamicMerger) Register(ch chan string) int {
 	m.locker.Lock()
 	defer m.locker.Unlock()
-	m.idgen++
-	token := m.idgen
-	selectCase := reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ch)}
-	m.selectsMap[token] = selectCase
-	m.selects = append(m.selects, selectCase)
-	return token
+	m.stopper <- struct{}{}
+	m.wg.Wait()
+	m.directRegister(ch)
+	m.Start()
+	return m.idgen
 }
 
 // регистрирует новый канал и возвращает его токн
 func (m *DynamicMerger) Unregister(token int) {
 	m.locker.Lock()
 	defer m.locker.Unlock()
+	m.stopper <- struct{}{}
+	m.wg.Wait()
 	delete(m.selectsMap, token)
-	newselects := make([]reflect.SelectCase,0)
+	newselects := make([]reflect.SelectCase, 0)
 	for _, c := range m.selectsMap {
 		newselects = append(newselects, c)
 	}
 
 	m.selects = newselects
+	m.Start()
 }
 
-
 func (m *DynamicMerger) Start() {
+	m.wg.Add(1)
 	// тут не стану заморачиваться за синхронизацию как в Pipe - там просто хотелось это отработать
-	go func() {
+	go func(){
+		defer m.wg.Done()
 		for {
-			m.locker.RLock()
+			select {
+				case <- m.stopper:
+					return
+			default:
+				break
+			}
 			_, value, ok := reflect.Select(m.selects)
 			if ok {
 				m.out <- value.String()
 			}
-			m.locker.RUnlock()
 		}
 	}()
 }
